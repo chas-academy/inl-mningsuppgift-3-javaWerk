15006f139694b64f60b8845e41e2261094e67871
javaWerk <johan@transformwerk.tech>
2025-05-16 09:59:01 +0200

    Removed console.logs, still works as expected on my machine (Mac, Chrome)

--- src/script.js
+++ src/script.js
@@ -4,12 +4,7 @@ const productPrice = document.getElementById("priceInput");
 const addButton = document.getElementById("addButton");
 const cartList = document.getElementById("cartList");
 
-//let currentChart = [ {product:"Gaskammare", price:340000,quantity:1} ];
 let currentChart = [];
-//let productExists = false;
-console.log(currentChart)
-
-
 
 addButton.addEventListener("click", () => {
 
@@ -21,18 +16,12 @@ addButton.addEventListener("click", () => {
     console.log("Exists: "+productExists);
 
 
-    
     if ( productExists ){
         //Kontrollerar om objektet redan finns i arrayen (genom att jämföra productName) – om det redan finns, öka quantity med 1:
         productExists.quantity += currProductObject.quantity;
-        console.log(currProductObject.quantity);
-        console.log(currProductObject.product + " Exists " + productExists)
-        console.log(currentChart);
     } else {
         //Om den inte finns, lägg till den i listan:
         currentChart.push(currProductObject);
-        console.log(currProductObject.product + " New " + productExists)
-        console.log(currentChart);
     }
     //Rensar input-fälten efter att en produkt har lagts till:
     productName.value = "";
@@ -43,20 +32,6 @@ addButton.addEventListener("click", () => {
     for (currentProd of currentChart){
         cartList.innerHTML = cartList.innerHTML + ( currentProd.quantity +" st "+ currentProd.product +" á: " + currentProd.price + "<br>" );
     }
-
-    //cartListing.push( prod.quantity +" "+ prod.product +" á: " + prod.price )
-
-    //currentChart.forEach(prod => console.log(prod.quantity +" "+ prod.product +" á: " + prod.price));
-
-    //x = currentChart.map(prodName => prodName.product, prodPrice => prodPrice.price);
-    //console.log( x )
-    //currentChart.forEach(prod => cartListing.push( prod.quantity +" "+ prod.product +" á: " + prod.price ));
-
-    //currentChart.forEach(prod => cartListing.push( (prod.quantity +" "+ prod.product +" á: " + prod.price).toString() ));
-
-    //console.log(typeof cartListing);
-
-    //cartList.innerHTML = cartListing;
     
 })
 
